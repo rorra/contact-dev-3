4b31755614a9d8d784e86654390ef0f3938c99e3
Rodrigo Dominguez <rorra@rorra.com.ar>
2010-08-18 18:58:48 -0300

    Assets configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ContactDev3::Application.initialize!
+
+
+# Assets
+ActionController::Base.asset_host = 'fisherhouse.org'
