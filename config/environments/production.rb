39eda9d9c10360ac81181e9b68045d0f2c7d44be
Rodrigo Dominguez <rorra@rorra.com.ar>
2010-08-19 18:34:44 -0300

    Assets hosts updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,5 +47,5 @@ ContactDev3::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_controller.asset_host = "http://assets.example.com"  
+  #config.action_controller.asset_host = "http://fisherhouse.org"  
 end
